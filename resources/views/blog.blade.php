d95d3afaee7444ee9ebc57ffe61d53d37646c8b4
Syahrul Safarudin Hasan <syahrul.safarudinh9@gmail.com>
2020-08-27 16:21:24 +0700

    adding new route and views

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar Laravel</title>
+</head>
+
+<body>
+    <h3>Syahrul Safarudin Hasan</h3>
+    <p>Hallo semua saya Syahrul, dan saya sedang belajar laravel</p>
+</body>
+
+</html>
