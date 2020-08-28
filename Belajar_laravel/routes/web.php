88e64f9413d684db58f468081b70ef2073c85d58
Syahrul Safarudin Hasan <syahrul.safarudinh9@gmail.com>
2020-08-28 14:27:22 +0700

    memisahkan folder belajar_laravel & membuat folder membuat crud

--- Belajar_laravel/routes/web.php
+++ Belajar_laravel/routes/web.php
@@ -25,8 +25,6 @@ Route::get('blog', function () {
 
 Route::get('dosen', 'DosenController@index');
 
-Route::get('/pegawai/{nama}', 'PegawaiController@index');
-
 Route::get('/formulir', 'PegawaiController@formulir');
 Route::post('/formulir/proses', 'PegawaiController@proses');
 
@@ -34,3 +32,6 @@ Route::post('/formulir/proses', 'PegawaiController@proses');
 Route::get('/blog', 'BlogController@home');
 Route::get('/blog/tentang', 'BlogController@tentang');
 Route::get('/blog/kontak', 'BlogController@kontak');
+
+// route CRUD
+Route::get('/pegawai', 'PegawaiController@index');
